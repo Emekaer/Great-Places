d747e93c76bcaf616d4758b68e669adf56a182cf
ER <emekaokaforer@gmail.com>
2020-04-18 22:44:35 +0100

    Updated

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,10 +1,26 @@
 export const ADD_PLACES = "ADD_PLACES";
-export const DELETE_PLACES ="DELETE_PLACES"
+export const DELETE_PLACES = "DELETE_PLACES";
+import * as FileSystem from "expo-file-system";
 
 export const addPlaces = (title, image) => {
-  return { type: ADD_PLACES, placeData: { title: title, imageUri: image} };
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    return { type: ADD_PLACES, placeData: { title: title, imageUri: newPath } };
+  };
 };
 
 export const deletePlaces = (id) => {
-  return { type: DELETE_PLACES, id :id };
+  return { type: DELETE_PLACES, id: id };
 };
